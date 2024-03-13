02c78b6e284ea605839dfc3f69de0ea3a10c4d3b
Miguel Kawakami <migskawakami@gmail.com>
2024-03-13 11:27:33 -0300

    start bmi calculator

--- app/src/main/java/com/example/contentdmoclass/MainActivity.kt
+++ app/src/main/java/com/example/contentdmoclass/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.contentdmoclass
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtName: TextView
+    lateinit var btnSend: Button
+    lateinit var txtPersonName: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        edtName = findViewById(R.id.etd_name)
+        btnSend = findViewById(R.id.btn_send)
+        txtPersonName = findViewById(R.id.person_name)
+
+        btnSend.setOnClickListener(View.OnClickListener {
+            val personName: String = edtName.text.toString()
+            txtPersonName.text = personName
+        })
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
